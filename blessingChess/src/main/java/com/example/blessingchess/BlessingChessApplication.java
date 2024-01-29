e87a6fab395700b60c4942dc632cad7bf2cc4f58
liuruiqi2005 <lruiqi53@gmail.com>
2024-01-30 00:04:53 +0800

    springboot项目

---
+++ blessingChess/src/main/java/com/example/blessingchess/BlessingChessApplication.java
@@ -0,0 +1,13 @@
+package com.example.blessingchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlessingChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlessingChessApplication.class, args);
+	}
+
+}
---
+++ blessingChess/src/test/java/com/example/blessingchess/BlessingChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blessingchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlessingChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
