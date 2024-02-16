a6c194353c7a82b8e26723429149190552cfa793
NickLin1024 <152240432+NickLin1024@users.noreply.github.com>
2024-02-16 20:26:45 +0800

    加了亿点点注释，方便巨佬们修改

--- src/main/java/com/example/BlessingChess/controller/GreetingCardController.java
+++ src/main/java/com/example/BlessingChess/controller/GreetingCardController.java
@@ -6,33 +6,68 @@ import com.example.BlessingChess.service.GreetingCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 贺卡控制器，用于处理与贺卡相关的HTTP请求。
+ */
 @RestController
 @RequestMapping("/greetingCard")
 public class GreetingCardController {
     @Autowired
     GreetingCardService greetingCardService;
 
+    /**
+     * 加入新的贺卡。
+     *
+     * @param greetingCardReceiver 贺卡的dto信息
+     * @return 操作结果，如果成功则返回OK状态
+     */
     @PostMapping("/send")
     public Result insertNewCard(GreetingCardReceiver greetingCardReceiver){
         return greetingCardService.insertNewCard(greetingCardReceiver);
     }
 
-
+    /**
+     * 根据用户ID查询所有该用户收到的贺卡。
+     *
+     * @param id 用户的ID
+     * @return 查询结果，包含贺卡信息的列表
+     */
     @GetMapping("/receive/{id}")
     public Result selectByReceiverId(@PathVariable Integer id){
         return greetingCardService.selectByReceiverId(id);
     }
 
+
+    /**
+     * 根据用户ID获取一个新贺卡
+     *
+     * @param id 用户的ID
+     * @return 查询结果，包含新贺卡的信息
+     */
     @GetMapping("{id}")
     public Result selectNewCard(@PathVariable Integer id){
         return greetingCardService.selectNewCard(id);
     }
 
+    /**
+     * 删除对应id的贺卡
+     *
+     * @param cardId 需要删除的贺卡id
+     * @return 删除的结果，若成功返回ok
+     */
     @DeleteMapping("{cardId}")
     public Result deleteCard(@PathVariable Integer cardId){
         return greetingCardService.deleteCard(cardId);
     }
 
+
+    /**
+     * 删除对应id的贺卡
+     *
+     * @param cardId 需要修改的贺卡id
+     * @param greetingCardReceiver 前端提供的修改信息，若无需修改的变量为null
+     * @return 删除的结果，若成功返回ok
+     */
     @PutMapping("{cardId}")
     public Result updateCard(@PathVariable Integer cardId,GreetingCardReceiver greetingCardReceiver){
         return greetingCardService.updateCard(cardId,greetingCardReceiver);
--- src/main/java/com/example/BlessingChess/service/GreetingCardService.java
+++ src/main/java/com/example/BlessingChess/service/GreetingCardService.java
@@ -7,32 +7,75 @@ import com.example.BlessingChess.mapper.GreetingCardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 贺卡服务类，提供对贺卡数据的相关操作。
+ *
+ * @author LXY LRQ
+ * @version 1.0
+ * @since 2024-02-16
+ */
 @Service
 public class GreetingCardService {
     //注入GreetingCardMapper
     @Autowired
     GreetingCardMapper greetingCardMapper;
 
+    /**
+     * 加入新的贺卡。
+     *
+     * @param greetingCardReceiver 贺卡的dto信息
+     * @return 操作结果，如果成功则返回OK状态
+     */
     public Result insertNewCard(GreetingCardReceiver greetingCardReceiver){
+        //TODO 加入代码确认发送者id的有效性、图标和背景是否存在
         GreetingCard greetingCard = new GreetingCard(greetingCardReceiver);
         greetingCardMapper.insertNewCard(greetingCard);
         return Result.ok();
     }
 
+    /**
+     * 根据用户ID查询所有该用户收到的贺卡。
+     *
+     * @param id 用户的ID
+     * @return 查询结果，包含贺卡信息的列表
+     */
     public Result selectByReceiverId(Integer id){
+        //TODO 健壮性！！
         return Result.success(greetingCardMapper.selectByReceiverId(id),"success");
     }
 
+    /**
+     * 根据用户ID获取一个新贺卡
+     *
+     * @param id 用户的ID
+     * @return 查询结果，包含新贺卡的信息
+     */
     public Result selectNewCard(Integer id){
+        //TODO selectNewCard方法是所有可选贺卡的集合，要从中随机选一个，然后用mapper中已经写好的方法，把这个新贺卡的获取信息放入中间表
         return Result.success(greetingCardMapper.selectNewCard(id),"success");
     }
 
+    /**
+     * 删除对应id的贺卡
+     *
+     * @param cardId 需要删除的贺卡id
+     * @return 删除的结果，若成功返回ok
+     */
     public Result deleteCard(Integer cardId){
+        //TODO 健壮性！！比如确定该贺卡是否存在
         greetingCardMapper.deleteCard(cardId);
         return Result.ok();
     }
 
+    /**
+     * 删除对应id的贺卡
+     *
+     * @param id 需要修改的贺卡id
+     * @param greetingCardReceiver 前端提供的修改信息，若无需修改的变量为null
+     * @return 删除的结果，若成功返回ok
+     */
     public Result updateCard(Integer id,GreetingCardReceiver greetingCardReceiver){
+        //TODO 健壮性！！比如确定该贺卡是否存在
         GreetingCard greetingCard = new GreetingCard(greetingCardReceiver);
         greetingCard.setId(id);
         greetingCardMapper.updateCard(greetingCard);
