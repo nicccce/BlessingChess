669567f620a10f10384a464c136b0aa568aee44d
nicccce <lin2832856512@163.com>
2024-10-19 14:47:27 +0800

    UPDATE
    更新

---
+++ src/main/java/com/example/BlessingChess/data/wechat/WeChat.java
@@ -0,0 +1,11 @@
+package com.example.BlessingChess.data.wechat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class WeChat {
+    public static String appId = "your-app-id";
+    public static String secret = "your-secret";
+}
